9443e7a96314db795be1eb5b0ddaeb45a3a2c59a
SURESHROKAYA <sureshrokaya761@gmail.com>
2023-08-24 09:37:31 +0545

    Footer section Responsive

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ darkBtn.onclick = function () {
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-teme")
 }
+
+var settingsmenu = document.querySelector('.settings-menu');
+
+function settingsmenuToggle()
+{
+    settingsmenu.classList.toggle('settings-menu-height');
+}
+
+var menu = document.querySelector('.menu-bar');
+
+function menuToggle()
+{
+    menu.classList.toggle('menu-bar-height');
+}
